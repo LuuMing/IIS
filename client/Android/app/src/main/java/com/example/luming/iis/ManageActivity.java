b36b66430aab31a150d74c7c114a2b7c4f8d0a37
LuMing <784315443@qq.com>
2018-11-28 17:01:52 +0800

    初步完成控制界面三个 page

--- client/Android/app/src/main/java/com/example/luming/iis/ManageActivity.java
+++ client/Android/app/src/main/java/com/example/luming/iis/ManageActivity.java
@@ -3,13 +3,20 @@ package com.example.luming.iis;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ManageActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    ViewPager viewPager;
+    BottomNavigationView navigationView;
+    MenuItem   menuItem;
+    List<Fragment> fragmentList = new ArrayList<>();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,13 +25,13 @@ public class ManageActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    viewPager.setCurrentItem(0);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    viewPager.setCurrentItem(1);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    viewPager.setCurrentItem(2);
                     return true;
             }
             return false;
@@ -35,10 +42,47 @@ public class ManageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage);
+        mSocket socket = mSocket.getInstance();
+        navigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        viewPager = findViewById(R.id.viewpager_launch);
+        setNavigation();
+    }
+    private void setNavigation()
+    {
+        navigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if(menuItem != null)
+                {
+                    menuItem.setChecked(false);
+                }
+                else {
+                        navigationView.getMenu().getItem(0).setChecked(false);
+                }
+                menuItem = navigationView.getMenu().getItem(position);
+                menuItem.setChecked(true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        Fragment1 fragment1 = new Fragment1();
+        Fragment2 fragment2 = new Fragment2();
+        Fragment3 fragment3 = new Fragment3();
+        fragmentList.add(fragment1);
+        fragmentList.add(fragment2);
+        fragmentList.add(fragment3);
+        BottomViewAdapter adapter = new BottomViewAdapter(getSupportFragmentManager(),fragmentList);
+        viewPager.setAdapter(adapter);
+        viewPager.setOffscreenPageLimit(3);
     }
 
 }
