6a74adf878ddaf28d5943be72e7536c611659c06
LuMing <784315443@qq.com>
2018-12-08 02:51:11 +0000

    fix bug

--- server/server.py
+++ server/server.py
@@ -18,7 +18,7 @@ class MyServer(socketserver.BaseRequestHandler):
         		config.write(configfile)
 		if sys.platform == 'win32':
 			os.system('tools\ini2json.exe config.ini')
-		else if sys.platform == 'linux2':
+		elif sys.platform == 'linux2':
 			os.system('./tools/ini2json_ARM config.ini')
                 else:
                         os.system('./tools/ini2json config.ini')
